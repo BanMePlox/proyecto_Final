50395cb89b02f4bbca2cb30060aa844b0903541c
Peter Parker <pedrojimenezlujan1@gmail.com>
2022-02-04 20:59:59 +0100

    inicio bbdd (a medias, solo producto y categoria)

--- database/migrations/2022_02_04_182107_create_categories_table.php
+++ database/migrations/2022_02_04_182107_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldsToUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class AddFieldsToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('nif');
-            $table->string('address')->default('No introducida');
-            $table->string('username')->unique();
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique;
+            $table->timestamps();
         });
     }
 
@@ -27,8 +27,6 @@ class AddFieldsToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('categories');
     }
 }
