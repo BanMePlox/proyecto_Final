01f29817e5adb9c2381188dfb36b535894fef211
Peter Parker <pedrojimenezlujan1@gmail.com>
2022-02-04 20:59:18 +0100

    traduccion

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Olvidaste tu contraseña? Introduce tu email y te enviaremos un correo para recuperar la contraseña') }}
         </div>
 
         @if (session('status'))
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Enviar el correo de reinicio de contraseña') }}
                 </x-jet-button>
             </div>
         </form>
