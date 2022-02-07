6f17e060dc550df999a4adf9ca84d0f84db5b64a
Peter Parker <pedrojimenezlujan1@gmail.com>
2022-02-07 17:34:27 +0100

    bbdd a medias

--- database/migrations/2022_02_07_155123_create_carts_table.php
+++ database/migrations/2022_02_07_155123_create_carts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateCartsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
-            $table->string('name');
-            $table->string('description');
-            $table->binary('image');
-            $table->decimal('price');
-            $table->integer('stock');
+            $table->integer('amount');
+            $table->unsignedBigInteger('product_id');
+            $table->date('order_date');
+            $table->unsignedDecimal('product_price');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('carts');
     }
 }
