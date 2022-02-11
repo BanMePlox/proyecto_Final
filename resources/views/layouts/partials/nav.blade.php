839f53972a01ed4726c8473c2a869bdccd46f53a
Peter Parker <pedrojimenezlujan1@gmail.com>
2022-02-11 15:45:14 +0100

    mucho commit no me acuerdo que he hecho ya

---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="cabecera">
+        <img src="img/logoJuanadona.png" alt="Logo" id="logo">
+        <div id="buscador">
+            <form action="">
+            <img src="img/lupa.png" alt="lupa">
+            <input type="text" placeholder="Busca tus productos">
+            </form>
+        </div>
+        <div class="linea__media">
+            <a href="#">Categorias</a>
+            <a href="#">Login</a>
+            <a href="#">Registrate</a>
+            <img src="img/carroVacio.png" alt="carrito" id="carro">
+        </div>
+    </nav>
+</header>
