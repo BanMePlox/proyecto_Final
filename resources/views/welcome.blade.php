defc2b768bbcb93efd116e5c188ea0f203da94c1
Peter Parker <pedrojimenezlujan1@gmail.com>
2022-02-11 18:59:30 +0100

    logo ha vuelto cambiamos welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,21 @@
     <article>
         <h2>Los mas vendidos</h2>
         <section class="mas__vendidos">
+            @forelse($products as $product)
             <div class="producto">
-                <img src="img/instagram.png" alt="Imagen Productos">
+                <img src="{{Storage::url('product/'.$product->file_path)}}" alt="Imagen productos">
                 <div class="producto__texto">
-                    <h3>Nombre producto</h3>
-                    <p>Precio</p>
+                    <h3>{{$product->name}}</h3>
+                    <p>{{$product->price}}€</p>
                     <a class="btn__producto" href="#">Añadir al carrito</a>
                 </div>
             </div>
+            @empty
+            aa
+            @endforelse
+            <br>
+{{-- Aqui terminan los mas vendidos --}}
+
             <div class="producto">
                 <img src="img/instagram.png" alt="Imagen Productos">
                 <div class="producto__texto">
